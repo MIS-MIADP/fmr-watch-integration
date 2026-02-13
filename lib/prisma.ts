a22de72ae23ad451ca9bf7daa692e93492e0d73c
MIS-MIADP <mis.miadp@gmail.com>
2026-02-13 13:15:25 +0800

    update prisma and title (#6)
    * update message foor seed request (#1)
    
    Co-authored-by: Ronie - MIADP <ronie.miadp@gmail.com>
    
    * Postinstall (#3)
    
    * update message foor seed request
    
    * added postinstall
    
    ---------
    
    Co-authored-by: Ronie - MIADP <ronie.miadp@gmail.com>
    
    * update prisma and title (#5)
    
    * update message foor seed request
    
    * added postinstall
    
    * updated prisma and title
    
    ---------
    
    Co-authored-by: Ronie - MIADP <ronie.miadp@gmail.com>
    
    ---------
    
    Co-authored-by: Ronie - MIADP <ronie.miadp@gmail.com>

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@/.config/prisma/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg';
 import "dotenv/config";
 
 const connectionString = `${process.env.DATABASE_URL}`
